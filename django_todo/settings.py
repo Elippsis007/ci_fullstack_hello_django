90a65924b4cebcec88e4e48ddd0e77b66d1fc954
Elippsis007 <michaeldrmm@gmail.com>
2021-12-07 18:44:33 +0000

    updated password

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4*%=u9@lcguqm90(dd&)m3b4#^)=rjve%(esi5i%oo9f=+&hd_'
+SECRET_KEY = 'postgres://zltmtsjbttdbcq:37b48524f16252bc0d7d18dea292b8d7413d142099e3b396efa2f4cc49e6db21@ec2-52-213-119-221.eu-west-1.compute.amazonaws.com:5432/d6hktim5k87sjo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
